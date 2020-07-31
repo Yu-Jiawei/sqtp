c1377c6b620ab1bcf52854a23f2a39086c9e7900
徐正浩 <smileandyxu@gmail.com>
2020-07-31 16:58:49 +0800

    前端描述

--- src/components/Central.vue
+++ src/components/Central.vue
@@ -2,6 +2,28 @@
   <v-app id="inspire">
     <v-content>
       <v-container
+      class="fill-height"
+      fluid>
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <div id="title">
+            <div style="position:relative;
+                        width:500px; margin:0 auto;"
+            class="title"
+            >
+              <h1>
+                计算机职业生涯规划
+              </h1>
+            </div>
+          </div>
+        </v-row>
+      </v-container>
+    </v-content>
+    
+    <v-content>
+      <v-container
         class="fill-height"
         fluid
       >
@@ -9,6 +31,18 @@
           align="center"
           justify="center"
         >
+          <div id="description">
+            <div style="position:relative;
+                        width:500px; margin:0 auto;"
+            class="grey--text"
+            >
+              本项目旨在帮助初入计算机专业的同学们规划自己将来的职业生涯。
+              输入你感兴趣的计算机职业（硬件、前端、数据库乃至全栈工程师）
+              点击查询，即可得到该职业需要的技能树，也可以了解到就业方向和
+              薪资水平等信息。希望这些信息可以帮助计算机专业的同学们更好地
+              综合规划自己的职业生涯。
+            </div>
+          </div>
           <v-col
             cols="12"
             sm="8"
